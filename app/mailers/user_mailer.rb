c4d45e5c2c1036709ec52be0524c2e90a6ce9d16
bhanuteja6 <bhanuteja6@gmail.com>
2012-10-12 11:40:00 +0530

    my changes

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ActionMailer::Base
+  default  :from => "bsurya7@gmail.com"
+
+	def registration_confirmation(user)
+	@user = user
+	
+		attachments["rails.png"] = File.read("#{Rails.root}/app/assets/images/rails.png")
+
+		mail(:to => @user.email, :subject => "Registered")
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kaminarigem::Application.config.secret_token = 'cb1b186f0286c62b06112656ea01bd46fcbc3762fdbfec7f45be4c637468a60835de325093a6c4439934ee7438f4ca85d24f7700a1507ab66edd1f27f0baca09'
